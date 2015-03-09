9a9fadb9b63d1668b286177fa6a54e0539ba885f
hardyboyz <hardi84@yahoo.com>
2015-03-09 16:09:02 +0800

    Update MOLPay.POS.class.php

--- MOLPay.POS.class.php
+++ MOLPay.POS.class.php
@@ -8,17 +8,16 @@
 ***/
 Class POS_MOLPay {
 	
-	var $url 	= 'https://www.onlinepayment.com.my/MOLPay/API/mobile_new/index.php';
 	var $params = array();
 	var $verify_key;	
 	
 	function __construct($params){
 		$this->params['merchant_id'] 		= $params['merchant_id'];
 		$this->params['merchant_order_id'] 	= $params['merchant_order_id'];
-		$this->params['bill_desc']			= $params['bill_desc'];
-		$this->params['channel']			= 'MOLWallet'; // DO NOT CHANGE THIS VALUE
+		$this->params['bill_desc']		= $params['bill_desc'];
+		$this->params['channel']		= 'MOLWallet'; // DO NOT CHANGE THIS VALUE
 		$this->params['bill_mobile']		= '60388888888'; // DO NOT CHANGE THIS VALUE
-		$this->params						= $params;
+		$this->params				= $params;
 		
 		$this->params['vcode'] 			= md5($this->params['amount'].$params['merchant_id'].$this->params['orderid'].									$params['verify_key']."E9");		
 		return $this->params;
@@ -45,23 +44,22 @@ Class POS_MOLPay {
 		$results = json_decode($result, 1);
 		
 		if($results['msgType'] == 'E10'){
-			$urlMobile = 'https://web.molwallet.com/MOLWallet-GTWs/APIs/index.php';
 			
 			$mobile['merchant_id']		= $this->params['merchant_id'];
-			$mobile['merchant_order_id']= $this->params['merchant_order_id'];
+			$mobile['merchant_order_id']	= $this->params['merchant_order_id'];
 			$mobile['terminal_id']		= $this->params['terminal_id'];
 			$mobile['wallet_id']		= $this->params['bill_mobile'];
-			$mobile['amount']			= $this->params['amount'];
+			$mobile['amount']		= $this->params['amount'];
 			$mobile['description']		= $this->params['bill_desc'];
-			$mobile['currency']			= $this->params['cur'];
-			$mobile['method']			= $this->params['method'];
-			$mobile['MsgID']			= $this->params['MsgID'];
+			$mobile['currency']		= $this->params['cur'];
+			$mobile['method']		= $this->params['method'];
+			$mobile['MsgID']		= $this->params['MsgID'];
 			
 			$data			= json_encode($mobile);
 			$datetime 		= time();
 			$checksum = SHA1( $data . md5($this->params['verify_key']) . $datetime);
 			
-			$postdataMobile				= array('params'=>json_encode($mobile),'checksum'=>$checksum,'appname'=>'MyCompany_Dev');
+			$postdataMobile			= array('params'=>json_encode($mobile),'checksum'=>$checksum,'appname'=>'MyCompany_Dev');
 			
 			$m 		= curl_init( $urlMobile );
 			curl_setopt( $m, CURLOPT_CUSTOMREQUEST, "POST");
@@ -83,7 +81,7 @@ Class POS_MOLPay {
 		$this->params['l_version']	= '2';
 		$this->params['app_name']	= $this->params['merchant_id'];
 		
-		$postdata 				= json_encode($this->params);
+		$postdata 			= json_encode($this->params);
 		
 		$ch 		= curl_init( $this->url );
 		curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -98,23 +96,22 @@ Class POS_MOLPay {
 		$results = json_decode($result, 1);
 		
 		if($results['msgType'] == 'E10'){
-			$urlMobile = 'https://web.molwallet.com/MOLWallet-GTWs/APIs/index.php';
 			
 			$mobile['merchant_id']		= $this->params['merchant_id'];
-			$mobile['merchant_order_id']= $this->params['merchant_order_id'];
+			$mobile['merchant_order_id']	= $this->params['merchant_order_id'];
 			$mobile['terminal_id']		= $this->params['terminal_id'];
 			$mobile['wallet_id']		= $this->params['bill_mobile'];
-			$mobile['amount']			= $this->params['amount'];
+			$mobile['amount']		= $this->params['amount'];
 			$mobile['description']		= $this->params['bill_desc'];
-			$mobile['currency']			= $this->params['cur'];
-			$mobile['method']			= $this->params['method'];
-			$mobile['MsgID']			= $this->params['MsgID'];
+			$mobile['currency']		= $this->params['cur'];
+			$mobile['method']		= $this->params['method'];
+			$mobile['MsgID']		= $this->params['MsgID'];
 			
 			$data			= json_encode($mobile);
 			$datetime 		= time();
 			$checksum = SHA1( $data . md5($this->params['verify_key']) . $datetime);
 			
-			$postdataMobile				= array('params'=>json_encode($mobile),'checksum'=>$checksum);
+			$postdataMobile		= array('params'=>json_encode($mobile),'checksum'=>$checksum);
 			
 			$m 		= curl_init( $urlMobile );
 			curl_setopt( $m, CURLOPT_CUSTOMREQUEST, "POST");
@@ -130,10 +127,9 @@ Class POS_MOLPay {
 	}
 	
 	function CheckInvoice($params){
-		$urlMobile = 'https://web.molwallet.com/MOLWallet-GTWs/APIs/Payment/InvoiceStatus';
 		
 		$mobile['merchant_id']		= $params['merchant_id'];
-		$mobile['merchant_order_id']= $params['merchant_order_id'];
+		$mobile['merchant_order_id']	= $params['merchant_order_id'];
 		
 		$data			= json_encode($mobile);
 		$datetime 		= time();
